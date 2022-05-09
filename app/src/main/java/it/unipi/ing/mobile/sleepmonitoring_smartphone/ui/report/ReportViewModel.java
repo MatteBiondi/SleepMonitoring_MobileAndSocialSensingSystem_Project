dafe72365bddec738c385d522610c51f8cf38777
MatteBiondi <matteobiondi1197@gmail.com>
2022-05-09 17:11:26 +0200

    First layout version

---
+++ app/src/main/java/it/unipi/ing/mobile/sleepmonitoring_smartphone/ui/report/ReportViewModel.java
@@ -0,0 +1,7 @@
+package it.unipi.ing.mobile.sleepmonitoring_smartphone.ui.report;
+
+import androidx.lifecycle.ViewModel;
+
+public class ReportViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/it/unipi/ing/mobile/sleepmonitoring_smartphone/ui/settings/SettingsViewModel.java
@@ -0,0 +1,7 @@
+package it.unipi.ing.mobile.sleepmonitoring_smartphone.ui.settings;
+
+import androidx.lifecycle.ViewModel;
+
+public class SettingsViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
