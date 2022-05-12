1123899e8fa92eeddcbfeb3ac5a48e3a4ef55960
MatteBiondi <matteobiondi1197@gmail.com>
2022-05-12 09:58:53 +0200

    Implemented the change of button image and the change of the onClickListener in wearapp

--- wearapp/src/main/java/it/unipi/ing/mobile/sleepmonitoring_watch/MainActivity.java
+++ wearapp/src/main/java/it/unipi/ing/mobile/sleepmonitoring_watch/MainActivity.java
@@ -2,7 +2,9 @@ package it.unipi.ing.mobile.sleepmonitoring_watch;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import it.unipi.ing.mobile.sleepmonitoring_watch.databinding.ActivityMainBinding;
@@ -13,6 +15,9 @@ public class MainActivity extends Activity {
     private TextView mTextView;
     private ActivityMainBinding binding;
     private SensorsManager sensorsManager;
+    private ImageButton play_stop_button;
+    private final String TAG = "MainActivity_LogTag";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +25,7 @@ public class MainActivity extends Activity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        play_stop_button = findViewById(R.id.play_stop_button);
 
         sensorsManager =new SensorsManager(this.getApplicationContext());
 
@@ -27,11 +33,26 @@ public class MainActivity extends Activity {
 
     public void start_recording(View view){
         try {
-            sensorsManager.registerListeners();
+            Log.i(TAG,"startRecording");
+            //todo register
+            //sensorsManager.registerListeners();
+            play_stop_button.setImageResource(R.drawable.ic_baseline_stop_circle);
+            play_stop_button.setOnClickListener(this::stop_recording);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+    public void stop_recording(View view){
+        try {
+            Log.i(TAG,"stopRecording");
+            //todo Unregister
+            //sensorsManager.unregisterListeners();
+            play_stop_button.setImageResource(R.drawable.ic_baseline_play_circle_filled);
+            play_stop_button.setOnClickListener(this::start_recording);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 }
