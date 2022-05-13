b91dfcd2f87fd5d5bce0ccdffb9c437174a5177e
MatteBiondi <matteobiondi1197@gmail.com>
2022-05-14 00:16:28 +0200

    Realized updating of welcome message and realized "see last report" feature

--- app/src/main/java/it/unipi/ing/mobile/sleepmonitoring_smartphone/ui/home/HomeFragment.java
+++ app/src/main/java/it/unipi/ing/mobile/sleepmonitoring_smartphone/ui/home/HomeFragment.java
@@ -1,20 +1,31 @@
 package it.unipi.ing.mobile.sleepmonitoring_smartphone.ui.home;
 
+import static android.content.Context.MODE_PRIVATE;
+
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 
+import it.unipi.ing.mobile.sleepmonitoring_smartphone.R;
 import it.unipi.ing.mobile.sleepmonitoring_smartphone.databinding.FragmentHomeBinding;
+import it.unipi.ing.mobile.sleepmonitoring_smartphone.ui.home.HomeFragmentDirections.ActionNavHomeToNavReport;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+
+    private String sharedPrefFile ;
+    private String user_first_name_preferences_key;
+    private SharedPreferences mPreferences;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,7 +35,26 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        // Set attributes for shared preferences
+        sharedPrefFile = getString(R.string.shared_preferences_file);
+        mPreferences=inflater.getContext().getSharedPreferences(sharedPrefFile, MODE_PRIVATE);
+        user_first_name_preferences_key = getString(R.string.user_first_name_preferences_key);
+
+        // Get account information from sharedPreferences
+        String firstName = mPreferences.getString(user_first_name_preferences_key, "");
+
         final TextView textView = binding.homeWelcomeLabel;
+        textView.append(" "+firstName);
+
+
+        // Set home fragment button listener for click event
+        Button seeLastReport = binding.homeLastReportButton;
+        seeLastReport.setOnClickListener(view -> {
+            ActionNavHomeToNavReport action =HomeFragmentDirections.actionNavHomeToNavReport();
+            action.setLastReport(true);
+            Navigation.findNavController(view).navigate(action);
+        });
+
         homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
--- app/src/main/java/it/unipi/ing/mobile/sleepmonitoring_smartphone/ui/report/ReportFragment.java
+++ app/src/main/java/it/unipi/ing/mobile/sleepmonitoring_smartphone/ui/report/ReportFragment.java
@@ -1,15 +1,19 @@
 package it.unipi.ing.mobile.sleepmonitoring_smartphone.ui.report;
 
+import static android.content.Context.MODE_PRIVATE;
+
 import androidx.lifecycle.ViewModelProvider;
 
 import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,6 +21,8 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 
 import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 import it.unipi.ing.mobile.sleepmonitoring_smartphone.R;
 
@@ -25,6 +31,8 @@ public class ReportFragment extends Fragment {
     private ReportViewModel mViewModel;
     private final String TAG = "ReportFragment";
     private EditText date;
+    private String sharedPrefFile;
+    private SharedPreferences mPreferences;
 
     public static ReportFragment newInstance() {
         return new ReportFragment();
@@ -34,37 +42,42 @@ public class ReportFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
+        mViewModel = new ViewModelProvider(this).get(ReportViewModel.class);
         View view = inflater.inflate(R.layout.fragment_report, container, false);
+
+        sharedPrefFile = getString(R.string.shared_preferences_file);
+        mPreferences=inflater.getContext().getSharedPreferences(sharedPrefFile, MODE_PRIVATE);
+
 
         date=view.findViewById(R.id.editText_date);
         date.setOnFocusChangeListener((v, hasFocus) -> {
             if(!hasFocus)
                 return;
             final Calendar cldr = Calendar.getInstance();
-            //Get today date
+            //Get current date
             int currentDay = cldr.get(Calendar.DAY_OF_MONTH);
             int currentMonth = cldr.get(Calendar.MONTH);
             int currentYear = cldr.get(Calendar.YEAR);
             // date picker dialog
             DatePickerDialog datepicker = new DatePickerDialog(getActivity(),
                     (dp, year, monthOfYear, dayOfMonth) -> date.setText(
-                            getString(R.string.report_date_format,
-                            dayOfMonth,
-                            monthOfYear + 1,
-                            year)),
+                            DateFormat.format("dd/MM/yyyy",
+                                    new GregorianCalendar(year,monthOfYear,dayOfMonth))),
                     currentYear, currentMonth, currentDay);
             datepicker.show();
             date.clearFocus();
         });
 
-        return view;
-    }
+        // Get args to check if the request of creation come from home fragment button or not
+        boolean fromHome = ReportFragmentArgs.fromBundle(getArguments()).getLastReport();
+        if(fromHome){
+            // Set the current date on the editText and update the plot
+            date.setText(DateFormat.format("dd/MM/yyyy", new Date()));
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(ReportViewModel.class);
-        // TODO: Use the ViewModel
+            //todo update the plot
+        }
+
+        return view;
     }
 
     @Override
