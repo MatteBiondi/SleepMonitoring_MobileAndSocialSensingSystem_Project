5d079ceca0a1509768941ada8ebbb2126a4012f6
Federico Cristofani <federico.cristofani98@outlook.it>
2022-05-17 11:36:06 +0200

    Database interface

---
+++ app/src/main/java/it/unipi/ing/mobile/sleepmonitoring_smartphone/database/SleepEventDatabase.java
@@ -0,0 +1,42 @@
+package it.unipi.ing.mobile.sleepmonitoring_smartphone.database;
+
+import android.content.Context;
+import androidx.room.Room;
+import java.util.List;
+
+public class SleepEventDatabase {
+    private static SleepEventDatabase instance = null;
+    private final SleepEventDB database;
+
+    public static SleepEventDatabase build(Context context){
+        if (instance == null){
+            instance = new SleepEventDatabase(context);
+        }
+        return instance;
+    }
+
+    private SleepEventDatabase(Context context){
+        database = Room.databaseBuilder(context, SleepEventDB.class, "sleep_event_db").build();
+    }
+
+    public List<SleepEvent> getAll(){
+        return database.sleep_event_dao().getAll();
+    }
+
+    public List<SleepEvent> getByDate(String date){
+        return database.sleep_event_dao().getByDate(date);
+    }
+
+    public void insertSleepEvents(SleepEvent... sleep_events){
+        database.sleep_event_dao().insertSleepEvents(sleep_events);
+    }
+
+    public void deleteBefore(String date){
+        database.sleep_event_dao().deleteBefore(date);
+    }
+
+    public void truncate(){
+        database.sleep_event_dao().truncate();
+    }
+
+}
